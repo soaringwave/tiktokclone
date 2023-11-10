66a34be933412a317583b7bc0a12244885c3f7c6
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-11-10 11:27:05 +0100

    Create project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TiktokApp());
+}
+
+class TiktokApp extends StatelessWidget {
+  const TiktokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tiktok Clone',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFE9435A),
+      ),
+      home: Container(),
+    );
+  }
+}
