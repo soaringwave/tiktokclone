e610886cd80e1cb619b15b2cda99e104fb4b1100
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-11-14 16:32:56 +0100

    Set main theme in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktokclone/constants/sizes.dart';
 import 'package:tiktokclone/features/authentication/sign_up_screen.dart';
 
 void main() {
@@ -13,7 +14,16 @@ class TiktokApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tiktok Clone',
       theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.white,
+            titleTextStyle: TextStyle(
+              fontSize: Sizes.size24,
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+            )),
         primaryColor: const Color(0xFFE9435A),
+        scaffoldBackgroundColor: Colors.white,
       ),
       home: const SignUpScreen(),
     );
