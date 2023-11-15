eedad1a2f6871f8c4c57839782457ea12d1a89e6
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-11-15 15:18:15 +0100

    Make EmailScreen

--- lib/features/authentication/email_screen.dart
+++ lib/features/authentication/email_screen.dart
@@ -1,1 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tiktokclone/constants/gaps.dart';
+import 'package:tiktokclone/constants/sizes.dart';
+import 'package:tiktokclone/features/authentication/widgets/form_button.dart';
 
+class EmailScreen extends StatefulWidget {
+  const EmailScreen({super.key});
+
+  @override
+  State<EmailScreen> createState() => _EmailScreenState();
+}
+
+class _EmailScreenState extends State<EmailScreen> {
+  final TextEditingController _emailControl = TextEditingController();
+
+  String _email = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _emailControl.addListener(() {
+      setState(() {
+        _email = _emailControl.text;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign up'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size40,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v24,
+            const Text(
+              'Input Email Address',
+              style: TextStyle(
+                fontSize: Sizes.size28,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Gaps.v24,
+            TextField(
+              controller: _emailControl,
+              cursorColor: Theme.of(context).primaryColor,
+              decoration: InputDecoration(
+                hintText: 'Email',
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Theme.of(context).hintColor,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Theme.of(context).hintColor,
+                  ),
+                ),
+              ),
+            ),
+            Gaps.v24,
+            FormButton(
+              abled: _email.isNotEmpty,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
