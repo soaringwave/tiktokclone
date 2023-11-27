cf0f425a608fae80835e44808acbebd12ac82e04
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-11-27 15:55:55 +0100

    Show and hide title by scrolling

--- lib/features/onboarding/interests_screen.dart
+++ lib/features/onboarding/interests_screen.dart
@@ -33,15 +33,46 @@ class InterestsScreen extends StatefulWidget {
 
 class _InterestsScreenState extends State<InterestsScreen> {
   final ScrollController _scrollController = ScrollController();
+  bool _showTitle = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (_scrollController.offset >= 90) {
+      if (_showTitle) return;
+      setState(() {
+        _showTitle = true;
+      });
+    } else {
+      if (_showTitle == false) return;
+      setState(() {
+        _showTitle = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Choose your interests',
-          style: TextStyle(
-            fontSize: Sizes.size20,
+        title: AnimatedOpacity(
+          opacity: _showTitle ? 1 : 0,
+          duration: const Duration(milliseconds: 250),
+          child: const Text(
+            'Choose your interests',
+            style: TextStyle(
+              fontSize: Sizes.size20,
+            ),
           ),
         ),
       ),
