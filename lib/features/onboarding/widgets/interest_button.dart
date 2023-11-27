ade469f6364c0c3c88c98b0c627d1a282cb0692d
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-11-27 16:11:18 +0100

    Change Button's style when clicked

--- lib/features/onboarding/widgets/interest_button.dart
+++ lib/features/onboarding/widgets/interest_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:tiktokclone/constants/sizes.dart';
 
-class InterestButton extends StatelessWidget {
+class InterestButton extends StatefulWidget {
   const InterestButton({
     super.key,
     required this.interest,
@@ -11,31 +11,52 @@ class InterestButton extends StatelessWidget {
   final String interest;
 
   @override
+  State<InterestButton> createState() => _InterestButtonState();
+}
+
+class _InterestButtonState extends State<InterestButton> {
+  bool _isSelected = false;
+
+  void _onTap() {
+    setState(() {
+      _isSelected = !_isSelected;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(
-          Sizes.size32,
+    return GestureDetector(
+      onTap: _onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 250),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(
+            Sizes.size32,
+          ),
+          color: _isSelected ? Theme.of(context).primaryColor : Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              blurRadius: 5,
+              spreadRadius: 3,
+            )
+          ],
         ),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.2),
-            blurRadius: 5,
-            spreadRadius: 3,
-          )
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: Sizes.size16,
-          vertical: Sizes.size14,
-        ),
-        child: Text(
-          interest,
-          style: const TextStyle(
-            fontSize: Sizes.size20,
-            fontWeight: FontWeight.w600,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size16,
+            vertical: Sizes.size14,
+          ),
+          child: AnimatedDefaultTextStyle(
+            duration: const Duration(milliseconds: 250),
+            style: TextStyle(
+              fontSize: Sizes.size20,
+              fontWeight: FontWeight.w600,
+              color: _isSelected ? Colors.white : Colors.black,
+            ),
+            child: Text(
+              widget.interest,
+            ),
           ),
         ),
       ),
