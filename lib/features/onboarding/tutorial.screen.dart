40bcc3440bbbf158ba76978ebd4c6ccd4122ed86
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-11-28 14:40:22 +0100

    Make TapBarView with DefaultTabController

--- lib/features/onboarding/tutorial.screen.dart
+++ lib/features/onboarding/tutorial.screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tiktokclone/constants/gaps.dart';
+import 'package:tiktokclone/constants/sizes.dart';
 
 class TutorialScreen extends StatefulWidget {
   const TutorialScreen({super.key});
@@ -10,6 +12,91 @@ class TutorialScreen extends StatefulWidget {
 class _TutorialScreenState extends State<TutorialScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        body: SafeArea(
+          child: TabBarView(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: Sizes.size40,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Gaps.v20,
+                    Text(
+                      'Choose your interests',
+                      style: TextStyle(
+                        fontSize: Sizes.size40,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Gaps.v10,
+                    Text(
+                      'This is a tutorial page.',
+                      style: TextStyle(
+                        fontSize: Sizes.size20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: Sizes.size40,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Gaps.v20,
+                    Text(
+                      'Page 2',
+                      style: TextStyle(
+                        fontSize: Sizes.size40,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Gaps.v10,
+                    Text(
+                      'This is a tutorial page.',
+                      style: TextStyle(
+                        fontSize: Sizes.size20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: Sizes.size40,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Gaps.v20,
+                    Text(
+                      'Page 3',
+                      style: TextStyle(
+                        fontSize: Sizes.size40,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Gaps.v10,
+                    Text(
+                      'This is a tutorial page.',
+                      style: TextStyle(
+                        fontSize: Sizes.size20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
