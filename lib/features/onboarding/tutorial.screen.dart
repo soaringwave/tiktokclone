fee1d10660d0ec35d8defcbfae4650c953cbf6ce
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-11-29 17:58:39 +0100

    Make tutorial pages with AnimatedCrossFade

--- lib/features/onboarding/tutorial.screen.dart
+++ lib/features/onboarding/tutorial.screen.dart
@@ -12,106 +12,61 @@ class TutorialScreen extends StatefulWidget {
 class _TutorialScreenState extends State<TutorialScreen> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        body: const SafeArea(
-          child: TabBarView(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: Sizes.size40,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Gaps.v20,
-                    Text(
-                      'Choose your interests',
-                      style: TextStyle(
-                        fontSize: Sizes.size40,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Gaps.v10,
-                    Text(
-                      'This is a tutorial page.',
-                      style: TextStyle(
-                        fontSize: Sizes.size20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: Sizes.size40,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Gaps.v20,
-                    Text(
-                      'Page 2',
-                      style: TextStyle(
-                        fontSize: Sizes.size40,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Gaps.v10,
-                    Text(
-                      'This is a tutorial page.',
-                      style: TextStyle(
-                        fontSize: Sizes.size20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: Sizes.size40,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Gaps.v20,
-                    Text(
-                      'Page 3',
-                      style: TextStyle(
-                        fontSize: Sizes.size40,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Gaps.v10,
-                    Text(
-                      'This is a tutorial page.',
-                      style: TextStyle(
-                        fontSize: Sizes.size20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          child: Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: Sizes.size20,
-              vertical: Sizes.size28,
+    return const Scaffold(
+      body: SafeArea(
+        child: AnimatedCrossFade(
+          firstChild: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Sizes.size40,
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                TabPageSelector(
-                  color: Colors.white,
-                  selectedColor: Theme.of(context).primaryColor,
+                Gaps.v20,
+                Text(
+                  'Choose your interests',
+                  style: TextStyle(
+                    fontSize: Sizes.size40,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Gaps.v10,
+                Text(
+                  'This is a tutorial page.',
+                  style: TextStyle(
+                    fontSize: Sizes.size20,
+                  ),
                 ),
               ],
             ),
           ),
+          secondChild: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Sizes.size40,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Gaps.v20,
+                Text(
+                  'Follow the rules',
+                  style: TextStyle(
+                    fontSize: Sizes.size40,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Gaps.v10,
+                Text(
+                  'This is a tutorial page.',
+                  style: TextStyle(
+                    fontSize: Sizes.size20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          crossFadeState: CrossFadeState.showFirst,
+          duration: Duration(milliseconds: 500),
         ),
       ),
     );
