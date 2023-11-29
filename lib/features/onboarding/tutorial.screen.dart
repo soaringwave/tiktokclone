2764d9770dfd222aba64267045d7378e0ff49b6f
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-11-29 18:09:29 +0100

    Add GestureDetector with _onPanUpdate function

--- lib/features/onboarding/tutorial.screen.dart
+++ lib/features/onboarding/tutorial.screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:tiktokclone/constants/gaps.dart';
 import 'package:tiktokclone/constants/sizes.dart';
 
+enum Directions { right, left }
+
 class TutorialScreen extends StatefulWidget {
   const TutorialScreen({super.key});
 
@@ -10,63 +12,79 @@ class TutorialScreen extends StatefulWidget {
 }
 
 class _TutorialScreenState extends State<TutorialScreen> {
+  Directions _direction = Directions.right;
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    print(details);
+    if (details.delta.dx < 0) {
+      setState(() {
+        _direction = Directions.right;
+      });
+    } else {
+      _direction = Directions.left;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: AnimatedCrossFade(
-          firstChild: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: Sizes.size40,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Gaps.v20,
-                Text(
-                  'Choose your interests',
-                  style: TextStyle(
-                    fontSize: Sizes.size40,
-                    fontWeight: FontWeight.w600,
+    return GestureDetector(
+      onPanUpdate: _onPanUpdate,
+      child: const Scaffold(
+        body: SafeArea(
+          child: AnimatedCrossFade(
+            firstChild: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: Sizes.size40,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Gaps.v20,
+                  Text(
+                    'Choose your interests',
+                    style: TextStyle(
+                      fontSize: Sizes.size40,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                ),
-                Gaps.v10,
-                Text(
-                  'This is a tutorial page.',
-                  style: TextStyle(
-                    fontSize: Sizes.size20,
+                  Gaps.v10,
+                  Text(
+                    'This is a tutorial page.',
+                    style: TextStyle(
+                      fontSize: Sizes.size20,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
+            secondChild: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: Sizes.size40,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Gaps.v20,
+                  Text(
+                    'Follow the rules',
+                    style: TextStyle(
+                      fontSize: Sizes.size40,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Gaps.v10,
+                  Text(
+                    'This is a tutorial page.',
+                    style: TextStyle(
+                      fontSize: Sizes.size20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            crossFadeState: CrossFadeState.showFirst,
+            duration: Duration(milliseconds: 500),
           ),
-          secondChild: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: Sizes.size40,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Gaps.v20,
-                Text(
-                  'Follow the rules',
-                  style: TextStyle(
-                    fontSize: Sizes.size40,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Gaps.v10,
-                Text(
-                  'This is a tutorial page.',
-                  style: TextStyle(
-                    fontSize: Sizes.size20,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          crossFadeState: CrossFadeState.showFirst,
-          duration: Duration(milliseconds: 500),
         ),
       ),
     );
